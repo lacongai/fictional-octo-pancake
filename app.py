296775991f12cd8bf3f7582842a126a157ec66fb
HenTaiz <thailanfreefire2000@gmail.com>
2025-10-01 15:20:14 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -659,6 +659,3 @@ def api_update():
     except Exception as e:
         app.logger.error(f"Error checking update: {str(e)}", exc_info=True)
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False, threaded=True)
